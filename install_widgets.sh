4f2f5defc2f52500493598812ab465a6a2e2be24
irtaylor <taylori@onid.oregonstate.edu>
2016-10-22 21:05:05 -0400

    installer creates and runs the mongo daemon

--- install_widgets.sh
+++ install_widgets.sh
@@ -2,4 +2,12 @@
 
 printf "\nInstalling the Widgets-App!\n\n"
 
+# create the location for the database to be housed locally
 mkdir mongodb
+cd mongodb
+
+# create the data folder to hold the database, then run the mongo daemon in the background (log file is mongod.log)
+mkdir data
+mongod --dbpath=data --fork --logpath mongod.log
+
+cd ..
